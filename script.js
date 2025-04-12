c9b7d0392d5c8af5471e71dcaa2c4986c0da384c
strstik <61824strestik@student.sstebrno.cz>
2025-04-12 22:50:00 +0200

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const slider = document.getElementById("slider");
-    const obrazek = document.getElementById("amaz");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const slider = document.getElementById("slider");
+//     const obrazek = document.getElementById("amaz");
 
-    slider.addEventListener("input", function () {
-        if (slider.value >= 70) {
-            obrazek.style.display = "block";
-        } else {
-            obrazek.style.display = "none";
-        }
-    });
-});
+//     slider.addEventListener("input", function () {
+//         if (slider.value >= 70) {
+//             obrazek.style.display = "block";
+//         } else {
+//             obrazek.style.display = "none";
+//         }
+//     });
+// });
