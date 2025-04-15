cb2041a87f5950af6b7cad39a61a2e393b706e5a
strstik <61824strestik@student.sstebrno.cz>
2025-04-15 11:58:54 +0200

    update

--- script.js
+++ script.js
@@ -39,3 +39,11 @@
 //   sideMenu.classList.toggle('active');
 // });
 
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
