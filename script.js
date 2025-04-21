c6026bf6d34dbf0690c902d96602355faac50206
strstik <61824strestik@student.sstebrno.cz>
2025-04-21 23:52:31 +0200

    update

--- script.js
+++ script.js
@@ -31,3 +31,11 @@
 //     }
 // });
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
