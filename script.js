6d0305dc3a1931b750f81ea44f7728cc426be65c
strstik <61824strestik@student.sstebrno.cz>
2025-04-21 23:25:22 +0200

    Update

--- script.js
+++ script.js
@@ -31,19 +31,3 @@
 //     }
 // });
 
-
-// const toggleBtn = document.getElementById('menu-toggle');
-// const sideMenu = document.getElementById('side-menu');
-
-// toggleBtn.addEventListener('click', () => {
-//   sideMenu.classList.toggle('active');
-// });
-
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
