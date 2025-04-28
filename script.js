1a4f0b60e8c4ecfde3837c3d1c06de35a7538900
strstik <61824strestik@student.sstebrno.cz>
2025-04-29 00:03:24 +0200

    update

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const slider = document.getElementById("slider");
-//     const obrazek = document.getElementById("amaz");
-
-//     slider.addEventListener("input", function () {
-//         if (slider.value >= 70) {
-//             obrazek.style.display = "block";
-//         } else {
-//             obrazek.style.display = "none";
-//         }
-//     });
-// });
-
-
-// const menuButton = document.getElementById('menu-button');
-// const sideMenu = document.getElementById('side-menu');
-// const closeButton = document.getElementById('close-button');
-
-// menuButton.addEventListener('click', () => {
-//     sideMenu.classList.add('open');
-// });
-
-// closeButton.addEventListener('click', () => {
-//     sideMenu.classList.remove('open');
-// });
-
-// // volitelně: kliknutí mimo menu ho taky zavře
-// document.addEventListener('click', (e) => {
-//     if (!sideMenu.contains(e.target) && !menuButton.contains(e.target)) {
-//         sideMenu.classList.remove('open');
-//     }
-// });
-
+// This is a simple JavaScript function to toggle the display of a menu
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
